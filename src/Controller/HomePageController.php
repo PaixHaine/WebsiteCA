4bc32fc0e2f7295bf3f06d4701a5b2090c193966
BouriauB <47318623+BouriauB@users.noreply.github.com>
2020-05-11 15:23:59 +0200

    created auth feature with login & logout routes

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     /**
-     * @Route("/", name="home_page")
+     * @Route("/", name="app_homepage")
      */
     public function index()
     {
